374de54e26e15ee529ffcb331d4bcea9fe54d7eb
mkoko8855 <mkoko8855@naver.com>
2023-07-23 20:35:00 +0900

    0723 front feat/LYS 사전 기능 구현 및 수정

--- react/src/component/search/SearchForm.js
+++ react/src/component/search/SearchForm.js
@@ -5,24 +5,54 @@ import { BsStar, BsStarFill } from "react-icons/bs";
 const MAX_DESCRIPTION_LENGTH = 25;
 const MAX_FAVORITES_COUNT = 5;
 
+const SelectedFavoriteDetails = ({ selectedFavorites }) => {
+  if (!selectedFavorites || selectedFavorites.length === 0) return null;
+
+  return (
+    <div className="selected-favorites">
+      {selectedFavorites.map((favorite) => (
+        <div key={favorite.id}>
+          <h4>{favorite.word}</h4>
+          <p>{favorite.definition}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const SearchForm = () => {
   const [searchWord, setSearchWord] = useState("");
   const [wordList, setWordList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const [isSearchPerformed, setIsSearchPerformed] = useState(false);
   const [expandedIndex, setExpandedIndex] = useState(-1);
   const [alreadyAdded, setAlreadyAdded] = useState(false);
+  const [selectedFavorite, setSelectedFavorite] = useState(null);
+  const [selectedFavorites, setSelectedFavorites] = useState([]);
+
   const [favorites, setFavorites] = useState(() => {
     const storedFavorites = localStorage.getItem("favorites");
     return storedFavorites ? JSON.parse(storedFavorites) : [];
   });
 
+  const handleFavoriteClick = (word) => {
+    setWordList([]);
+    setErrorMessage("");
+    setSearchWord("");
+
+    const isAlreadySelected = selectedFavorites.some(
+      (favWord) => favWord.id === word.id
+    );
+
+    if (!isAlreadySelected) {
+      setSelectedFavorites((prevSelected) => [...prevSelected, word]);
+    }
+  };
+
   const handleSearch = async () => {
     try {
       setIsLoading(true);
       setErrorMessage("");
-      setIsSearchPerformed(true);
 
       if (searchWord === "") {
         setWordList([]);
@@ -65,18 +95,36 @@ const SearchForm = () => {
   };
 
   useEffect(() => {
-    if (searchWord === "") {
-      setErrorMessage("");
-    } else {
-      handleSearch();
-    }
-  }, [searchWord]);
+    const initialStarFillStatus = {};
+    favorites.forEach((favWord, index) => {
+      initialStarFillStatus[getWordId(favWord, index)] = true;
+    });
+    setStarFillStatus(initialStarFillStatus);
+  }, [favorites]);
 
   const toggleDescription = (index) => {
     setExpandedIndex((prevIndex) => (prevIndex === index ? -1 : index));
   };
 
+  const clearSelectedFavorite = () => {
+    setSelectedFavorite(null);
+  };
+
   const getWordId = (word, index) => `${word.word}-${index}`;
+
+  const [starFillStatus, setStarFillStatus] = useState({});
+
+  useEffect(() => {
+    setSelectedFavorite(null);
+  }, [searchWord]);
+
+  useEffect(() => {
+    if (selectedFavorites.length > MAX_FAVORITES_COUNT) {
+      setSelectedFavorites((prevSelected) =>
+        prevSelected.slice(-MAX_FAVORITES_COUNT)
+      );
+    }
+  }, [selectedFavorites]);
 
   const addToFavorites = (word, index) => {
     setFavorites((prevFavorites) => {
@@ -89,6 +137,12 @@ const SearchForm = () => {
         const newFavorites = [...prevFavorites, { ...word, id: wordId }];
         localStorage.setItem("favorites", JSON.stringify(newFavorites));
         setAlreadyAdded(false);
+
+        setStarFillStatus((prevStatus) => ({
+          ...prevStatus,
+          [wordId]: true,
+        }));
+
         return newFavorites;
       } else {
         setAlreadyAdded(true);
@@ -101,15 +155,37 @@ const SearchForm = () => {
     setAlreadyAdded(false);
   };
 
-  const removeFromFavorites = (word) => {
+  const removeFromFavorites = (word, index) => {
     setFavorites((prevFavorites) =>
-      prevFavorites.filter((favWord) => favWord.id !== word.id)
+      prevFavorites.filter((favWord) => {
+        if (favWord.id === word.id) {
+          if (selectedFavorite && selectedFavorite.id === word.id) {
+            clearSelectedFavorite();
+          }
+
+          const wordId = getWordId(word, index);
+          setStarFillStatus((prevStatus) => {
+            const updatedStatus = { ...prevStatus };
+            delete updatedStatus[wordId];
+            return updatedStatus;
+          });
+
+          return false;
+        }
+        return true;
+      })
+    );
+
+    setAlreadyAdded(false);
+    localStorage.setItem(
+      "favorites",
+      JSON.stringify(favorites.filter((favWord) => favWord.id !== word.id))
     );
   };
 
   const isWordInFavorites = (word, index) => {
     const wordId = getWordId(word, index);
-    return favorites.some((favWord) => favWord.id === wordId);
+    return starFillStatus[wordId];
   };
 
   const sortedWordList = wordList.sort((a, b) => a.word.localeCompare(b.word));
@@ -139,15 +215,12 @@ const SearchForm = () => {
         <h3>즐겨찾기 목록</h3>
         <div className="added-favorites ml-auto">
           {favorites.map((favWord) => (
-            <div key={favWord.id}>
+            <div key={favWord.id} className="mb-1">
               <BsStarFill
                 className="favorite-icon"
                 onClick={() => removeFromFavorites(favWord)}
               />
-              <a
-                href={`#${favWord.word}`}
-                onClick={() => setSearchWord(favWord.word)}
-              >
+              <a href="#" onClick={() => handleFavoriteClick(favWord)}>
                 <span>{favWord.word}</span>
               </a>
             </div>
@@ -164,89 +237,102 @@ const SearchForm = () => {
         </div>
       )}
 
+      <SelectedFavoriteDetails selectedFavorites={selectedFavorites} />
+
       {errorMessage ? (
         <p className="error-message">{errorMessage}</p>
-      ) : sortedWordList.length > 0 ? (
-        <div className="search-here">
-          <p className="search-results">검색 결과: {sortedWordList.length}개</p>
-          <ul className="search-results-list">
-            {sortedWordList.map((word, index) => {
-              const uniqueId = `${word.word}-${index}`;
-              const starFilled = isWordInFavorites(word, index);
+      ) : (
+        <>
+          {sortedWordList.length > 0 && !selectedFavorite && (
+            <div className="search-here">
+              <p className="search-results">
+                검색 결과: {sortedWordList.length}개
+              </p>
+              <ul className="search-results-list">
+                {sortedWordList.map((word, index) => {
+                  const uniqueId = `${word.word}-${index}`;
+                  const starFilled = starFillStatus[getWordId(word, index)];
 
-              return (
-                <li key={index}>
-                  <strong>{word.word} : </strong>
-                  {word.definition.length > MAX_DESCRIPTION_LENGTH ? (
-                    <div>
-                      {expandedIndex === index ? (
-                        <>
+                  return (
+                    <li key={index}>
+                      <strong>{word.word} : </strong>
+                      {word.definition.length > MAX_DESCRIPTION_LENGTH ? (
+                        <div>
+                          {expandedIndex === index ? (
+                            <>
+                              {word.definition}
+                              <span
+                                className="collapse-toggle"
+                                onClick={() => toggleDescription(index)}
+                              >
+                                접기
+                              </span>
+                            </>
+                          ) : (
+                            <>
+                              {word.definition.slice(0, MAX_DESCRIPTION_LENGTH)}
+                              ...
+                              <span
+                                className="expand-toggle"
+                                onClick={() => toggleDescription(index)}
+                              >
+                                더 보기
+                              </span>
+                            </>
+                          )}
+                          {starFilled ? (
+                            <BsStarFill
+                              className="favorite-icon"
+                              onClick={() => removeFromFavorites(word, index)}
+                              style={{ color: "red" }}
+                            />
+                          ) : (
+                            <BsStar
+                              className="favorite-icon"
+                              onClick={() => addToFavorites(word, index)}
+                            />
+                          )}
+                        </div>
+                      ) : (
+                        <div>
                           {word.definition}
+                          {starFilled ? (
+                            <BsStarFill
+                              className="favorite-icon"
+                              onClick={() => removeFromFavorites(word, index)}
+                              style={{ color: "red" }}
+                            />
+                          ) : (
+                            <BsStar
+                              className="favorite-icon"
+                              onClick={() => addToFavorites(word, index)}
+                            />
+                          )}
                           <span
                             className="collapse-toggle"
-                            onClick={() => toggleDescription(index)}
+                            style={{ display: "none" }}
                           >
                             접기
                           </span>
-                        </>
-                      ) : (
-                        <>
-                          {word.definition.slice(0, MAX_DESCRIPTION_LENGTH)}...
                           <span
                             className="expand-toggle"
-                            onClick={() => toggleDescription(index)}
+                            style={{ display: "none" }}
                           >
                             더 보기
                           </span>
-                        </>
+                        </div>
                       )}
-                      {starFilled ? (
-                        <BsStarFill
-                          className="favorite-icon"
-                          onClick={() => removeFromFavorites(word, index)}
-                        />
-                      ) : (
-                        <BsStar
-                          className="favorite-icon"
-                          onClick={() => addToFavorites(word, index)}
-                        />
-                      )}
-                    </div>
-                  ) : (
-                    <div>
-                      {word.definition}
-                      {starFilled ? (
-                        <BsStarFill
-                          className="favorite-icon"
-                          onClick={() => removeFromFavorites(word)}
-                        />
-                      ) : (
-                        <BsStar
-                          className="favorite-icon"
-                          onClick={() => addToFavorites(word)}
-                        />
-                      )}
-                      <span
-                        className="collapse-toggle"
-                        style={{ display: "none" }}
-                      >
-                        접기
-                      </span>
-                      <span
-                        className="expand-toggle"
-                        style={{ display: "none" }}
-                      >
-                        더 보기
-                      </span>
-                    </div>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      ) : (
-        searchWord && <p className="no-results">검색 결과가 없습니다.</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+
+          {!selectedFavorite && sortedWordList.length === 0 && searchWord && (
+            <p className="no-results">검색 결과가 없습니다.</p>
+          )}
+        </>
       )}
     </div>
   );
